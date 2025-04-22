f763ce1515897be8ac8f5eb980728b7b3f096c83
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-04-22 17:24:38 -0300

    Part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projeto-cards';
+  buttonClicked() {
+    console.log('Button clicked')
+  }
 }
